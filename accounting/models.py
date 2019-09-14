9c87285787a8f85d4a7a16ea2d2a5d8dcafd517a
Jonathan Lewis <jonathandavidlewis@gmail.com>
2019-09-14 14:02:17 -0700

    adds transactions and prevents negative cred/deb values

--- accounting/models.py
+++ accounting/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from enum import Enum
+
 
 # Create your models here.
 
@@ -26,3 +28,28 @@ class Account(models.Model):
 
     def __str__(self):
         return self.__unicode__()
+
+
+class Transaction(models.Model):
+    memo = models.CharField(max_length=200, blank=False)
+    ref_number = models.CharField(max_length=200, blank=False)
+    createdate = models.DateTimeField('date created', auto_now_add=True)
+    transactiondate = models.DateTimeField('transaction date', auto_now_add=True)
+
+    def __unicode__(self):
+        return '{}'.format(self.memo)
+
+    def __str__(self):
+        return self.__unicode__()
+
+
+class Credit(models.Model):
+    value = models.DecimalField(max_digits=20, decimal_places=2, validators=[MinValueValidator(0)])
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+
+class Debit(models.Model):
+    value = models.DecimalField(max_digits=20, decimal_places=2, validators=[MinValueValidator(0)])
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
